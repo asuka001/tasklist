149dd15df2ee6b70724c2f0bbb51cdac0645aa8e
asuka001 <a.asuka54@gmail.com>
2019-06-09 11:55:28 +0000

    fix login access

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :edit, :new, :show, :create, :update, :destroy]
+  before_action :require_user_logged_in
   before_action :set_task, only: [:edit, :show, :update, :destroy]
+  before_action :correct_user, only: [:destroy, :show]
   
   def index
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -52,6 +53,13 @@ class TasksController < ApplicationController
   
   def params_task
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
 end
